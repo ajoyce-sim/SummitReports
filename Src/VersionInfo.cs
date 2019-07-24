79ec2dee27ce1ff4219351bb3ad651791fa4d47d
Ricky Vega <git@noctusoft.com>
2019-07-24 07:41:41 -0500

    Deployed 1.0.14 nuget

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.13.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.13.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.14.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.14.0")]
