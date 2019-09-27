f884ba68dade9510ef20ec8d7735aea352c34014
Ricky Vega <git@noctusoft.com>
2019-09-27 10:17:55 -0500

    1.0.29 nuget update

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.28.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.28.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.29.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.29.0")]
