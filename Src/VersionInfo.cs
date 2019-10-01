a757b9a0fb86a8381844a1227b32c982a9d39d17
Ricky Vega <git@noctusoft.com>
2019-10-01 13:28:59 -0500

    Nuget deploy 1.0.33

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.32.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.32.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.33.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.33.0")]
