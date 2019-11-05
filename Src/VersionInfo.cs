bd5e5830289b01fcc65e00cae7e4e8492d4fc008
Ricky Vega <git@noctusoft.com>
2019-11-05 09:36:16 -0600

    update nuget

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.35.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.35.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.36.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.36.0")]
