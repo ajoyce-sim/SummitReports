0a03c72239925c5aba617ab0c578602c8c1b7bef
Ricky Vega <git@noctusoft.com>
2020-02-21 09:58:00 -0600

    Added enum string fix so Fixed Payment renders correctly on the excel sheet

--- Src/SummitReports.Infrastructure/IAmortizationCalculator.cs
+++ Src/SummitReports.Infrastructure/IAmortizationCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using SummitReports.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,41 @@ namespace SummitReports.Infrastructure
 {
     public enum eInterestCalculationMethodology
     {
+        [Description("Days 365")]
         Days365,
+        [Description("Days 360")]
         Days360
     }
     public enum ePrincipalCalculation
     {
+        [Description("Amortization")]
         Amortization,
+        [Description("Interest Only")]
         InterestOnly,
+        [Description("Fixed Payment")]
         FixedPayment
     }
+
+    public static class MyEnumExtensions
+    {
+        public static string ToDescriptionString(this ePrincipalCalculation val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+               .GetType()
+               .GetField(val.ToString())
+               .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+        public static string ToDescriptionString(this eInterestCalculationMethodology val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+               .GetType()
+               .GetField(val.ToString())
+               .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+
     public interface IAmortizationScheduleItem
     {
         int Month { get; set; }
