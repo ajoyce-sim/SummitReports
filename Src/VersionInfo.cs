3678f0a5199948574c4fdb88d9d2eb682bf9ce72
Ricky Vega <git@noctusoft.com>
2020-02-26 19:32:52 -0700

    Updated and NuGet deployment

--- Src/VersionInfo.cs
+++ Src/VersionInfo.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.43.*")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.43.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.45.*")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.45.0")]
