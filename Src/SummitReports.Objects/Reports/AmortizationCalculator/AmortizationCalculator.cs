904c9b4a9a617dac667eb24bb3ac11bd554e6124
Ricky Vega <git@noctusoft.com>
2020-03-20 11:49:54 -0500

    Added the Book and changes to the amort calculator

--- Src/SummitReports.Objects/Reports/AmortizationCalculator/AmortizationCalculator.cs
+++ Src/SummitReports.Objects/Reports/AmortizationCalculator/AmortizationCalculator.cs
@@ -31,21 +31,30 @@ namespace SummitReports.Objects
     public class AmortizationCalculator : SummitExcelReportBaseObject, IAmortizationCalculatorReport
     {
         public decimal UPB { get; set; } = 0.0m;
-        public decimal CurrentlyPastDueInterest { get; set; } = 0.0m;
-        public decimal CurrentlyPastDueFeesAndAdvances { get; set; } = 0.0m;
-        public decimal PerDiemInterest { get; set; } = 0.0m;
-        public decimal SIMFeesAndAdvances { get; set; } = 0.0m;
-        public DateTime StartDate { get; set; } = DateTime.Now;
-        public eInterestCalculationMethodology InterestCalculationMethodology { get; set; } = eInterestCalculationMethodology.Days365;
-        public ePrincipalCalculation PrincipalCalculation { get; set; } = ePrincipalCalculation.Amortization;
-        public decimal FixedPaymentAmount { get; set; } = 0.0m;
         public decimal InterestRate { get; set; } = 0.0m;
+        public eInterestCalculationMethodology InterestCalculationMethodology { get; set; } = eInterestCalculationMethodology.Days365;
         public int AmortizationTermYears { get; set; } = 0;
         public int BalloonPaymentMonths { get; set; } = 0;
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public int FirstPaymentMonth { get; set; } = 1;
+        public decimal FixedPaymentAmount { get; set; } = 0.0m;
+        private int paymentFrequency = 1;
+        public int PaymentFrequency {
+            get { return paymentFrequency; }
+            set {
+                var validValues = new List<int> { 1,2,4,6,12 };
+                if (!validValues.Contains(value)) throw new ArgumentOutOfRangeException(string.Format("PaymentFrequency {0} is not 1,2,4,6,12", value));
+                paymentFrequency = value;
+            }
+        }
+        public bool IsInterestOnly { get; set; } = false;
+        public int PIKStartMonth { get; set; } = 0;
+        public int PIKEndMonth { get; set; } = 0;
+        public int InterestOnlyEnd { get; set; } = 0;
+        public bool IsFixedPayment { get; set; } = false;
 
         public AmortizationCalculator() : base(@"AmortizationCalculator\AmortizationCalculator_20200211.xlsx")
         {
-
         }
 
         List<IAmortizationScheduleItem> IAmortizationCalculatorReport.Calculate()
@@ -60,38 +69,43 @@ namespace SummitReports.Objects
                 if (this.FixedPaymentAmount <= 0) Errors.Add(string.Format("FixedPaymentAmount value of {0} is invalid.", this.FixedPaymentAmount));
                 if (this.InterestRate <= 0) Errors.Add(string.Format("InterestRate value of {0} is invalid.", this.InterestRate));
                 if (this.AmortizationTermYears <= 0) Errors.Add(string.Format("AmortizationTermYears value of {0} is invalid.", this.AmortizationTermYears));
-                if (this.BalloonPaymentMonths <= 0) Errors.Add(string.Format("BalloonPaymentMonths value of {0} is invalid.", this.BalloonPaymentMonths));
+                if (this.BalloonPaymentMonths < 0) Errors.Add(string.Format("BalloonPaymentMonths value of {0} is invalid.", this.BalloonPaymentMonths));
+                if (this.InterestOnlyEnd > 0) Errors.Add(string.Format("InterestOnlyEnd value of {0} is invalid.", this.InterestOnlyEnd));
+                if (this.PIKEndMonth > 0) Errors.Add(string.Format("PIKEndMonth value of {0} is invalid.", this.PIKEndMonth));
+                if (this.PIKStartMonth > 0) Errors.Add(string.Format("PIKStartMonth value of {0} is invalid.", this.PIKStartMonth));
                 if (Errors.Count>0)
                 {
                     throw new ArgumentException(string.Join(" ", Errors.ToArray()));
                 }
-                sheet.SetCellValue(2, "E", this, "UPB");
-                sheet.SetCellValue(3, "E", this, "CurrentlyPastDueInterest");
-                sheet.SetCellValue(4, "E", this, "CurrentlyPastDueFeesAndAdvances");
-                sheet.SetCellValue(6, "E", this, "PerDiemInterest");
-                sheet.SetCellValue(7, "E", this, "SIMFeesAndAdvances");
-                sheet.SetCellValue(10, "E", this, "StartDate");
-                sheet.SetCellValue(12, "E", this.InterestCalculationMethodology.ToDescriptionString());
-                sheet.SetCellValue(14, "E", this.PrincipalCalculation.ToDescriptionString());
-                sheet.SetCellValue(16, "E", this, "FixedPaymentAmount");
-                sheet.SetCellValue(18, "E", this, "InterestRate");
-                sheet.SetCellValue(19, "E", this, "AmortizationTermYears");
-                sheet.SetCellValue(20, "E", this, "BalloonPaymentMonths");
+                sheet.SetCellValue(10, "F", this, "UPB");
+                sheet.SetCellValue(12, "F", this, "InterestRate");
+                sheet.SetCellValue(14, "F", this.InterestCalculationMethodology.ToDescriptionString());
+                sheet.SetCellValue(15, "F", this, "AmortizationTermYears");
+                sheet.SetCellValue(16, "F", this, "BalloonPaymentMonths");
+                sheet.SetCellValue(17, "F", this, "StartDate");
+                sheet.SetCellValue(18, "F", this, "FirstPaymentMonth");
+                sheet.SetCellValue(21, "F", this, "PaymentFrequency");
+                sheet.SetCellValue(22, "F", this.IsInterestOnly );
+                sheet.SetCellValue(23, "F", this, "PIKStartMonth");
+                sheet.SetCellValue(24, "F", this, "PIKEndMonth");
+                sheet.SetCellValue(25, "F", this, "InterestOnlyEnd");
+                sheet.SetCellValue(26, "F", this.IsFixedPayment );
+                sheet.SetCellValue(27, "F", this, "FixedPaymentAmount");
 
                 if (workbook is XSSFWorkbook)
                     XSSFFormulaEvaluator.EvaluateAllFormulaCells(workbook);
                 else
                     HSSFFormulaEvaluator.EvaluateAllFormulaCells(workbook);
                 var result = new AmortizationScheduleResult();
-                var row = 23;
+                var row = 35;
                 for (int i = row; i < 500; i++)
                 {
                     var endBalance = sheet.GetCellValue(i, "J", 0.0m);
                     result.Add(new AmortizationScheduleItem()
                     {
-                        Month = sheet.GetCellValue(i, "B", 0),
-                        ItemDate = sheet.GetCellValue(i, "C", DateTime.MinValue),
-                        Factor = sheet.GetCellValue(i, "D", 0.0m),
+                        Month = sheet.GetCellValue(i, "C", 0),
+                        ItemDate = sheet.GetCellValue(i, "D", DateTime.MinValue),
+                        Factor = 0,
                         BeginningBalance = sheet.GetCellValue(i, "E", 0.0m),
                         Payment = sheet.GetCellValue(i, "F", 0.0m),
                         Principal = sheet.GetCellValue(i, "G", 0.0m),
---
+++ Src/SummitReports.Objects/Reports/SummitInvestorBook/SummitInvestorBookPdf.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using IronPdf;
+using NPOI.XWPF.UserModel;
+using SummitReports.Infrastructure;
+
+namespace SummitReports.Objects
+{
+    public class SummitInvestorBookPdf : SummitPDFReportBaseObject, IGenericReport
+    {
+        public SummitInvestorBookPdf() : base(@"SummitInvestorBook\SummitInvestorBookPdf.html")
+        {
+        }
+        /// <summary>
+        /// This will generate a Report with the Real Estate Comment.
+        /// </summary>
+        /// <param name="uwRECollateralId">uwRECollateralId</param>
+        /// <returns>Name of the file generated</returns>
+        public async Task<string> GenerateAsync(int uwRECollateralId)
+        {
+            try
+            {
+                if (!ReloadTemplate()) throw new Exception("Template could not be loaded :(");
+
+                string sSQL = "";
+                DataSet retDataSet = null;
+
+                sSQL = @"SET ANSI_WARNINGS OFF; SELECT * FROM [UW].[vw_CollateralRE] WHERE [uwRECollateralId] = @p0;";
+
+                retDataSet = await MarsDb.QueryAsDataSetAsync(sSQL, uwRECollateralId);
+                if ((retDataSet.Tables.Count == 1) && (retDataSet.Tables[0].Rows.Count == 1))
+                {
+                    var data = retDataSet.Tables[0].Rows[0];
+                    Document.ReplaceFieldValue(data, "RptHeader");
+                    Document.ReplaceFieldValue(data, "OneLineAddress");
+                    Document.ReplaceFieldValue(data, "SIMValue", "C0");
+                    Document.ReplaceFieldValue(data, "Comments");
+                    SaveToFile(GeneratedFileName);
+                    return GeneratedFileName;
+                }
+                return "No records found";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
